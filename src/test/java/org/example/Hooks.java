01b69a9e629fba8a416006d81a2fbd6f7fb88156
savan5 <savan.bathani123@gmail.com>
2022-11-21 01:38:58 +0000

    frame work

--- src/test/java/org/example/Hooks.java
+++ src/test/java/org/example/Hooks.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.example.driver.DriverManager;
+
 public class Hooks {
+    DriverManager driverManager = new DriverManager();
+
+
+
+@Before
+    public void setup() throws IllegalAccessException {
+        driverManager.runOnLocalBrowser();
+        driverManager.gotoUrl();
+        driverManager.maxBrowser();
+        driverManager.applyImplicitWait();
+
+    }
+    @After
+    public void tearDown(){
+        driverManager.closeBrowse();
+
+    }
+
 }
--- src/test/java/org/example/RunCukesTest.java
+++ src/test/java/org/example/RunCukesTest.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources",
+        dryRun = false,
+        strict = false,
+        tags = "login",
+        plugin = "json:target/cucumber-report/cucumber.json")
+
 public class RunCukesTest {
+    // a json report is generated as target/cucumber-report
+    //open maven tab and click 'execute maven goal'
+    //enter 'cluecumber-report:reporting' and click ok to build
+    //report will generated in target folder as generated-report
+    //to run from intellij
 }
